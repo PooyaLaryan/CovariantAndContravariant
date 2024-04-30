437e20108e5d49ccaf47f53ec179c9dc164e3d2f
P.Lariyan <P.Lariyan@P-PLARIYAN>
2024-04-30 11:06:27 +0330

    Add project files.

---
+++ CovariantAndContravariant/AllClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovariantAndContravariant
+{
+    internal interface IFruit { }
+    internal class Apple : IFruit { }
+    internal class Orange : IFruit { }
+    internal interface IX<out T> where T : IFruit { }
+    internal class X<T> : IX<T> where T : IFruit { }   
+
+    internal class Test
+    {
+        public void GetIxFruit(IX<IFruit> fruit)
+        {
+
+        }
+    }
+}
---
+++ CovariantAndContravariant/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CovariantAndContravariant;
+using System.Net.Http.Headers;
+
+var test = new Test();
+var apple = new X<Apple>();
+var orange = new X<Orange>();
+
+test.GetIxFruit(apple);
+test.GetIxFruit(orange);
+
+
+// Covariant : out 
+// Parent <- Child 
+IEnumerable<string> strings = new List<string>();
+IEnumerable<object> objects = strings;
+
+//Contravariant : in 
+//Child <- Parent
+
+void SetObject(object obj) { }
+Action<object> objAction = SetObject;
+Action<string> stringAction = objAction;
+
